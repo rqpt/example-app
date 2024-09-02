3f90c57932fbbae8160c09d59088ef1733008274
Ernst <ernst@cubezoo.joburg>
2024-09-02 20:51:39 -0200

    Split layout into component

---
+++ resources/views/components/layouts/app.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+      content="width=device-width, initial-scale=1.0">
+
+    <!--jquery-->
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"
+      integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo="
+      crossorigin="anonymous"></script>
+
+    <!-- pico css for some base styling -->
+    <link rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.classless.jade.min.css">
+
+
+    <title>Teruza</title>
+  </head>
+  <body>
+
+    {{ $slot }}
+
+  </body>
+</html>
