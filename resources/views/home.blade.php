f57e24c287bad50f85187e3dd7af728de7c43d9b
Ernst <ernst@cubezoo.joburg>
2024-09-02 18:33:03 -0200

    Scaffold view

---
+++ resources/views/home.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+      content="width=device-width, initial-scale=1.0">
+
+    <!--jquery-->
+    <script src="https://code.jquery.com/jquery-3.7.1.slim.min.js"
+      integrity="sha256-kmHvs0B+OpCW5GVHUNjv9rOmY0IvSIRcf7zGUDTDQM8="
+      crossorigin="anonymous"></script>
+
+      <!-- pico css for some base styling -->
+      <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.classless.min.css"
+      >
+
+    <title>Teruza</title>
+  </head>
+  <body>
+    <main>
+      Hello world
+    </main>
+  </body>
+</html>
