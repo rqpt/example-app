4423885cb9dc5a20bac5b3902c5e57042d70baa5
Pieter Ernst Vermeulen <76003847+rqpt@users.noreply.github.com>
2024-09-03 07:50:51 +0200

    Update app.blade.php

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -27,6 +27,8 @@
     <script defer
       src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
 
+    @vite('resources/css/app.css')
+      
     <title>Teruza</title>
   </head>
   <body>
