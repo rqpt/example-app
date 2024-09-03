1157f580dc6ba94e652766b642fefd9b5593e6e5
Ernst <ernst@cubezoo.joburg>
2024-09-03 13:48:51 -0200

    Split homepage into more subcomponents

---
+++ resources/views/components/reference-table.blade.php
@@ -0,0 +1,20 @@
+<section>
+  <hgroup>
+    <h2>Reference Table</h2>
+    <p>Current exchange rate to USD</p>
+  </hgroup>
+  <table>
+    <thead>
+      <tr>
+        <th>Currency Code</th>
+        <th>Rate</th>
+      </tr>
+    </thead>
+    <tbody>
+
+      <!--The table body here will be populated by the ajax call-->
+      <!--Please refer to resources/js/app.js-->
+
+    </tbody>
+  </table>
+</section>
