cbd8b1534e69196f6c17f83a0814058763cfc6e4
Ernst <ernst@cubezoo.joburg>
2024-09-04 09:52:54 -0200

    Refactor validation into separate class

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CurrencyConversionRequest;
 use App\Models\Currency;
 use Illuminate\Database\Eloquent\Collection;
-use Illuminate\Http\Request;
 use Illuminate\View\View;
 
 class SiteController
@@ -27,20 +27,15 @@ class SiteController
     /**
      * Our conversion method. We find the currency record by the
      * code column, and then do our conversion from there.
+     *
+     * Please see the App\Http\Requests\CurrencyConversionRequest for validations.
      */
-    public function convertCurrency(Request $request): string|false
+    public function convertCurrency(CurrencyConversionRequest $request): string|false
     {
-        $request->validate(
-            [
-                // As the currencies table is not directly managed by us, we need to specify
-                // the fully qualified model class path for the exists validation rule
-                'code' => ['required', 'string', 'size:3', 'exists:App\Models\Currency,code'],
-                'amount' => ['required', 'numeric', 'gte:0'],
-            ]
-        );
+        $validated = $request->validated();
 
-        $exchange = Currency::whereCode($request->code)->first();
+        $exchange = Currency::whereCode($validated['code'])->first();
 
-        return $exchange->convert($request->amount);
+        return $exchange->convert($validated['amount']);
     }
 }
